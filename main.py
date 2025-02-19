1f831a0d792df6c31d45644119843a49b025d0cb
Erickwi <electrictiesto@gmail.com>
2025-02-19 09:42:22 -0500

    fix experimental_rerun

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import streamlit as st
 def main():
     st.title("🏠 Bienvenido al Mapa Doctrinario del Ejército")
     st.write("Usa los botones para navegar por las opciones.")
+    
+    # Centrar la imagen
     st.image("cede.png", width=200)  # Tamaño pequeño de la imagen
+    
+    # Centrar los botones
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         if st.button("Mapa Doctrinario"):
